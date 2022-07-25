81dda3376725f37cac542468e2a37a1689d3408d
aclecio cruz <aclecioscruz@gmail.com>
2022-07-25 07:20:14 -0300

    implementação do conceito de herança e polimorfismo.

--- src/br/com/aclecio/desafio/dominio/Curso.java
+++ src/br/com/aclecio/desafio/dominio/Curso.java
@@ -1,31 +1,14 @@
 package br.com.aclecio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-    private String titulo;
-
-    private String descricao;
 
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
 
     public Curso() {
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -38,9 +21,14 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
