de2923c70b985dec4484d64d7a62f0ad19e0e51b
aclecio cruz <aclecioscruz@gmail.com>
2022-07-25 06:57:46 -0300

    implementação, mentoria e curso.

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.aclecio.desafio.dominio.Curso;
+import br.com.aclecio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição cursa Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("Descrição curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
